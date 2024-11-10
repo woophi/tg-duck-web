f2d88862c6f305bd49a3f5e9a053ad12de44405a
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-11-10 09:34:58 +0100

    added test api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { retrieveLaunchParams } from '@tma.js/sdk-react';
-import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 const { initDataRaw } = retrieveLaunchParams();
 
-function App() {
-  const [count, setCount] = useState(0);
+const getPayment = () => {
+  axios.post('https://gift-api.webapp-project.ru/api/tg/shop/stars', null, {
+    headers: {
+      Authorization: `Bearer ${initDataRaw}`,
+    },
+  });
+};
 
+function App() {
   console.debug(initDataRaw);
 
   return (
@@ -23,7 +29,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        <button onClick={getPayment}>call api</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
